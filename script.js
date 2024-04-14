64df8e03ab7734ed5ad20c2992536a265f6a2526
Krishang <112581575+krishang-r@users.noreply.github.com>
2024-04-14 16:33:48 +0530

    adding scroll eventlistener

--- script.js
+++ script.js
@@ -15,3 +15,41 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Error fetching source content:', error));
 });
+
+window.addEventListener('scroll', function() {
+    var main = document.getElementById('main');
+    var mainPosition = main.getBoundingClientRect().top;
+    var about = document.getElementById('about');
+    var aboutPosition = about.getBoundingClientRect().top;
+    var skill = document.getElementById('skills');
+    var skillPosition = skill.getBoundingClientRect().top;
+    var project = document.getElementById('projects');
+    var projectPosition = project.getBoundingClientRect().top;
+    var exp = document.getElementById('work-ex');
+    var expPosition = exp.getBoundingClientRect().top;
+    var edu = document.getElementById('edu');
+    var eduPosition = edu.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    const allItems = document.querySelectorAll(".navmenu ul li a");
+
+    // Remove active class from all navigation items
+    for(var i = 0; i < allItems.length; i++) {
+        allItems[i].classList.remove('active');
+    }
+
+    // Check which section is in view and add active class to its corresponding navigation item
+    if (mainPosition < windowHeight && mainPosition > 0) {
+        allItems[0].classList.add("active");
+    } else if (aboutPosition < windowHeight && aboutPosition > 0) {
+        allItems[0].classList.add("active");
+    } else if (skillPosition < windowHeight && skillPosition > 0) {
+        allItems[1].classList.add("active");
+    } else if (projectPosition < windowHeight && projectPosition > 0) {
+        allItems[2].classList.add("active");
+    } else if (expPosition < windowHeight && expPosition > 0) {
+        allItems[3].classList.add("active");
+    } else if (eduPosition < windowHeight && eduPosition > 0) {
+        allItems[4].classList.add("active");
+    }
+});
